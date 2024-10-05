47b478c1ced404277b195ba77cebaef91ea51115
Dipesh Acharya <dipesh@awecode.com>
2024-10-05 22:29:25 +0545

    chore: add tsx and prettier config

---
+++ src/core/db.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Client } from 'pg'
+
+console.log(process.env.POSTGRES_URL)
+
+const client = new Client({
+  connectionString: process.env.POSTGRES_URL,
+})
+
+export const db = drizzle(client)
