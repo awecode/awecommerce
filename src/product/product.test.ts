9de170f8dcac81b993ba4660bd8a7e63c19bd862
Dipesh Acharya <dipesh@awecode.com>
2024-10-06 08:57:18 +0545

    chore: use faker for tests

--- src/product/product.test.ts
+++ src/product/product.test.ts
@@ -1,14 +1,18 @@
 import { expect, it } from 'vitest'
 import { productService } from './service'
+import { faker } from '@faker-js/faker'
 
 it('should create a product', async () => {
+  const name = faker.commerce.productName()
+  const description = faker.commerce.productDescription()
+  const price = faker.number.int({ min: 10, max: 100 })
   const product = await productService.createProduct({
-    name: 'Test Product',
-    description: 'Test Description',
-    price: 100,
+    name,
+    description,
+    price,
   })
   expect(product).toBeDefined()
-  expect(product.name).toBe('Test Product')
-  expect(product.description).toBe('Test Description')
-  expect(product.price).toBe(100)
+  expect(product.name).toBe(name)
+  expect(product.description).toBe(description)
+  expect(product.price).toBe(price)
 })
