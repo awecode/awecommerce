78e29df2a3e8125030a48937f982c8ca0f37182f
Dipesh Acharya <dipesh@awecode.com>
2024-10-08 19:13:10 +0545

    fix: fix test setup for sqlite

--- config.ts
+++ config.ts
@@ -3,8 +3,8 @@ import { type PgDatabase } from './core/db/pg/db'
 
 type Dialect = 'pg' | 'sqlite'
 
-// export const dialect: Dialect = 'sqlite'
-// export type dialectType = LibSQLDatabase
+export const dialect: Dialect = 'sqlite'
+export type dialectType = LibSQLDatabase
 
-export const dialect: Dialect = 'pg'
-export type dialectType = PgDatabase
+// export const dialect: Dialect = 'pg'
+// export type dialectType = PgDatabase
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,16 +1,27 @@
-import { PGlite } from '@electric-sql/pglite'
 import { drizzle as drizzlePglite } from 'drizzle-orm/pglite'
 import { migrate as migratePglite } from 'drizzle-orm/pglite/migrator'
+import { migrate as migrateSqlite } from 'drizzle-orm/libsql/migrator'
 import path from 'path'
 import { afterAll, beforeAll } from 'vitest'
 import { getClient } from '../core/db/pg/db'
+import { initializeDb } from '../core/db/sqlite/db'
+
+import { dialect, type dialectType } from '../config'
+import { type LibSQLDatabase } from 'drizzle-orm/libsql'
 
 const doMigrate = async () => {
-  const client = getClient()
-  const drizzle = drizzlePglite(client)
-  await migratePglite(drizzle, {
-    migrationsFolder: path.join(process.cwd(), 'migrations'),
-  })
+  if (dialect === 'pg') {
+    const client = getClient()
+    const drizzle = drizzlePglite(client)
+    await migratePglite(drizzle, {
+      migrationsFolder: path.join(process.cwd(), 'migrations'),
+    })
+  } else {
+    const db = initializeDb() as unknown as LibSQLDatabase
+    await migrateSqlite(db, {
+      migrationsFolder: path.join(process.cwd(), 'migrations'),
+    })
+  }
 }
 
 beforeAll(async () => {
@@ -18,6 +29,8 @@ beforeAll(async () => {
 })
 
 afterAll(async () => {
-  const client = getClient()
-  await client.end()
+  if (dialect === 'pg') {
+    const client = getClient()
+    await client.end()
+  }
 })
