3603f2ca930b7b031e21c2729d4bf95406c69cf5
Rohan Dhimal <drowhann@gmail.com>
2025-01-28 10:58:24 +0545

    refactor: remove unused migration for order_log table

--- migrations/pg/0018_gorgeous_iron_patriot.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "order_log" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"orderId" integer NOT NULL,
-	"log" text NOT NULL,
-	"createdAt" timestamp with time zone DEFAULT now() NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "order_log" ADD CONSTRAINT "order_log_orderId_order_id_fk" FOREIGN KEY ("orderId") REFERENCES "public"."order"("id") ON DELETE no action ON UPDATE no action;
