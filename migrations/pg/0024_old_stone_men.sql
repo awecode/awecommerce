4fd88ad2b3d79d9184c351c4195d29879e007fc8
Rohan Dhimal <drowhann@gmail.com>
2025-02-07 16:12:00 +0545

    feat: rename shipping address fields for consistency in schema

---
+++ migrations/pg/0024_old_stone_men.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "shipping_address" RENAME COLUMN "country_code" TO "countryCode";--> statement-breakpoint
+ALTER TABLE "shipping_address" RENAME COLUMN "line_1" TO "line1";--> statement-breakpoint
+ALTER TABLE "shipping_address" RENAME COLUMN "line_2" TO "line2";--> statement-breakpoint
+ALTER TABLE "shipping_address" RENAME COLUMN "house_no" TO "houseNo";--> statement-breakpoint
+ALTER TABLE "shipping_address" RENAME COLUMN "postal_code" TO "postalCode";
