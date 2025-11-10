d5c5ec29c8edb2d4416514bc0378017be940d0df
Rohan Dhimal <drowhann@gmail.com>
2025-11-10 11:54:21 +0545

    feat: add createdByUserId field to orderStatusChange schema for tracking user actions

--- apps/order/schemas/pg.ts
+++ apps/order/schemas/pg.ts
@@ -85,6 +85,7 @@ export const orderStatusChanges = pgTable('order_status_change', {
   previousStatus: orderStatusEnum().notNull(),
   newStatus: orderStatusEnum().notNull(),
   createdAt: timestamp({ mode: 'string', withTimezone: true }).notNull().defaultNow(),
+  createdByUserId: text(),
 })
 
 export const orderLogs = pgTable('order_log', {
